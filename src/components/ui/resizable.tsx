1f200a74fd83dc08063ea6706d6f82ea3f967efc
Alejandro Casado <alejandroacasadoc@gmail.com>
2026-06-17 18:29:54 -0400

    Update ResizableHandle background color to primary

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -47,7 +47,7 @@ function ResizableHandle({
       {...props}
     >
       {withHandle && (
-        <div className="z-10 flex h-6 w-1 shrink-0 rounded-lg bg-primary/60" />
+        <div className="z-10 flex h-6 w-1 shrink-0 rounded-lg bg-primary" />
       )}
     </ResizablePrimitive.Separator>
   )
